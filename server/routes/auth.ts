cfdae67e02dae9753a281ee901c8e8003f62123d
WookiesRpeople2 <keentobiansky@gmail.com>
2024-12-18 14:05:13 +0100

    Chore: add role to frontend

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { authValidation } from "../middleware/authValidation";
-import { signIn, validateToken } from "../controllers/user";
+import { signIn, userUpdate, validateToken } from "../controllers/user";
+import { requireAuth } from "../middleware/requireAuth";
 
 export const authRouter = express.Router();
 
 authRouter.post("/sign-in", authValidation, signIn);
 
-authRouter.post("/validate-token", validateToken)
+authRouter.post("/edit", requireAuth, userUpdate);
+
+authRouter.post("/validate-token", validateToken);
