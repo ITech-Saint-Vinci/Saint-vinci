fec19dbb2594794a01f96a9b0d11c77688d7b343
WookiesRpeople2 <keenantobiansky@gmail.com>
2024-12-19 16:41:19 +0100

    Fixed validation in the database

--- server/models/student.ts
+++ server/models/student.ts
@@ -3,25 +3,26 @@ import { model, now, Schema } from "mongoose";
 export const Students = model(
   "Student",
   new Schema({
-    firstName:{
-        type: String,
-        require: true
+    firstName: {
+      type: String,
+      require: true,
+      unique: true,
     },
     lastName: {
-        type: String,
-        require: true
+      type: String,
+      require: true,
     },
     birthdate: {
-        type: Date
+      type: Date,
     },
-    isReapeating:{
-        type: Boolean,
-        default: false
+    isReapeating: {
+      type: Boolean,
+      default: false,
     },
     createdAt: {
-        type: Date,
-        default: now()
+      type: Date,
+      default: now(),
     },
-    class: { type: Schema.Types.ObjectId, ref: 'Class' }
+    class: { type: Schema.Types.ObjectId, ref: "Class" },
   })
 );
